16493be57f20dd76d67d018a3b5ac0176f7f3b7e
XiaoyueZhanginNCL <zhangxiaoyue2022@gmail.com>
2024-02-24 00:23:50 +0000

    login-html+css

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,37 @@
-const Login=()=>{
-    return (
-        <div>this is login</div>
-    )
-    
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
+const Login = () => {
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form validateTrigger='onBlur'>
+          <Form.Item name="username"
+            rules={[
+                { required: true, 
+                  message: 'Please input your username!' 
+                },
+                { pattern:/^1[3-9]\d{9}$/,
+                  message: 'Please enter the correct phone number format!'
+                }]}>
+            <Input size="large" placeholder="Please enter your mobile phone number..." />
+          </Form.Item>
+          <Form.Item name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}>
+            <Input size="large" placeholder="Please enter the verification code..." />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
-export default Login;
+export default Login
