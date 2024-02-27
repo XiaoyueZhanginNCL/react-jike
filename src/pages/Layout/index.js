1520c0b180fbdab41a005d0df3cfd8d58a276d6a
XiaoyueZhanginNCL <zhangxiaoyue2022@gmail.com>
2024-02-27 15:42:23 +0000

    hightlight menu

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -6,28 +6,38 @@ import {
   LogoutOutlined,
 } from '@ant-design/icons'
 import './index.scss'
+import { Outlet, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const { Header, Sider } = Layout
 
 const items = [
   {
     label: '首页',
-    key: '1',
+    key: '/',
     icon: <HomeOutlined />,
   },
   {
     label: '文章管理',
-    key: '2',
+    key: '/article',
     icon: <DiffOutlined />,
   },
   {
     label: '创建文章',
-    key: '3',
+    key: '/publish',
     icon: <EditOutlined />,
   },
 ]
 
 const GeekLayout = () => {
+  const navigate=useNavigate();
+  const location=useLocation();
+  const selectedKey=location.pathname;//拿到当前路由路径
+  function onMenuClicked(values){
+    // console.log(values)
+    const path=values.key;
+    navigate(path);
+  }
   return (
     <Layout>
       <Header className="header">
@@ -46,12 +56,14 @@ const GeekLayout = () => {
           <Menu
             mode="inline"
             theme="dark"
-            defaultSelectedKeys={['1']}
+            selectedKeys={selectedKey}
             items={items}
+            onClick={onMenuClicked}
             style={{ height: '100%', borderRight: 0 }}></Menu>
         </Sider>
         <Layout className="layout-content" style={{ padding: 20 }}>
-          内容
+          {/* 二级路由出口 */}
+          <Outlet></Outlet>
         </Layout>
       </Layout>
     </Layout>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,28 @@ import Layout from "../pages/Layout";
 import Login from "../pages/Login";
 import { createBrowserRouter } from 'react-router-dom'
 import { AuthRoute } from "@/components/AuthRoute";
+import Publish from '@/pages/Publish'
+import Article from '@/pages/Article'
+import Home from '@/pages/Home'
 
 const router=createBrowserRouter([
     {
         path:'/',
-        element:<AuthRoute><Layout /></AuthRoute>
+        element:<AuthRoute><Layout /></AuthRoute>,
+        children:[
+            {
+                index:true,
+                element:<Home />
+            },
+            {
+                path:'article',
+                element:<Article />
+            },
+            {
+                path:'publish',
+                element:<Publish />
+            }
+        ]
     },
     {
         path:'/login',
